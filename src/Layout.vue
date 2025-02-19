44c6ea23e3e30bebeb09b8f7f49f81194a11336b
Junk-debug <maks.naumovich@gmail.com>
2025-02-19 22:14:50 +0100

    feat: add UI components for card, button, input, label, and switch and sonner(toast), add `WavyBackground` component

---
+++ src/Layout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useColorMode } from "@vueuse/core";
+import { Toaster } from "@/components/ui/sonner";
+import WavyBackground from "@/components/WavyBackground.vue";
+
+import type { HtmlHTMLAttributes } from "vue";
+import { cn } from "./lib/utils";
+
+const props = defineProps<{ class: HtmlHTMLAttributes["class"] }>();
+
+const mode = useColorMode();
+
+if (localStorage.getItem("vueuse-color-scheme") === "auto") {
+  mode.value = "dark";
+}
+</script>
+<template>
+  <Toaster richColors />
+  <WavyBackground
+    :class="cn(props.class)"
+    :backgroundFill="mode === 'light' ? '#fff' : '#000'"
+  >
+    <slot />
+  </WavyBackground>
+</template>
---
+++ src/components/ui/sonner/Sonner.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { useColorMode } from "@vueuse/core";
+import type { ToasterProps } from "vue-sonner";
+import { Toaster as Sonner } from "vue-sonner";
+
+const { theme, ...props } = defineProps<ToasterProps>();
+const { value } = useColorMode();
+</script>
+
+<template>
+  <Sonner
+    class="toaster group"
+    :theme="theme || (value === 'auto' ? 'system' : value)"
+    v-bind="props"
+    :toast-options="{
+      classes: {
+        toast:
+          'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+        description: 'group-[.toast]:text-muted-foreground',
+        actionButton:
+          'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+        cancelButton:
+          'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+      },
+    }"
+  />
+</template>
