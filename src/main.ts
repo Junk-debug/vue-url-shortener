4dc4c2721c853f3feb4221d2085cc031a5f1c0ef
Junk-debug <maks.naumovich@gmail.com>
2025-02-19 22:12:17 +0100

    init: initialize project, configure styles and fonts, add deps,

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import "./styles/globals.css";
+import "@fontsource/geist-sans/300.css";
+import "@fontsource/geist-sans/400.css";
+import "@fontsource/geist-sans/500.css";
+import "@fontsource/geist-sans/600.css";
+import "@fontsource/geist-sans/700.css";
+import "@fontsource/geist-sans/800.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
